f45a18215373c13d5d65b4b967f7fa731ca25a93
Anthony Bui <76550986+anthonybui94@users.noreply.github.com>
2021-01-26 15:27:52 -0600

    Section 12 completed

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,36 +1,71 @@
 import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import Translate from './components/Translate';
+// import Dropdown from './components/Dropdown';
+// import Accordion from './components/Accordion';
+// import Search from './components/Search';
 
-const items = [
-  {
-    title: 'What is React?',
-    content: 'React is a front end javascript framework'
-  },
-  {
-    title: 'Why use React?',
-    content: 'React is a favorite JS library among engineers'
-  },
-  {
-    title: 'How do you use React?',
-    content: 'You use React by creating components'
-  }
-]
+// const items = [
+//   {
+//     title: 'What is React?',
+//     content: 'React is a front end javascript framework'
+//   },
+//   {
+//     title: 'Why use React?',
+//     content: 'React is a favorite JS library among engineers'
+//   },
+//   {
+//     title: 'How do you use React?',
+//     content: 'You use React by creating components'
+//   }
+// ]
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-	render() {
-		return (
-      <div>
-        <Accordion items={items} />
-        <br/>
-        <Search />
-      </div>
-    );
-	}
+// const options = [
+//   {
+//     label: 'The Color Red',
+//     value: 'red'
+//   },
+//   {
+//     label: 'The Color Green',
+//     value: 'green'
+//   },
+//   {
+//     label: 'A Shade of Blue',
+//     value: 'blue'
+//   },
+//   {
+//     label: 'A Shade of Gray',
+//     value: 'gray'
+//   },
+//   {
+//     label: 'The Color Black',
+//     value: 'black'
+//   }
+// ];
+
+const App = () => {
+
+  return (
+    <div style={{ margin:'20px' }}>
+      <Translate />
+    </div>
+  );
 }
 
 export default App;
+
+
+
+// Dropdown Menu Stuff
+// {/* <button
+// className="ui button"
+// onClick={() => setShowDropdown(!showDropdown)}>
+// Toggle Dropdown
+// </button>
+// {showDropdown ? <Dropdown 
+// options={options} 
+// selected={selected}
+// onSelectedChange={setSelected} 
+// /> : null}
+// <span style={{ color: selected.value }}>
+// Almost before we knew it, we had left the ground.
+// </span> */}
---
+++ widgets/src/components/Convert.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import config from '../apis/config.js';
+
+const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('')
+  const [debouncedText, setDebouncedText] = useState(text)
+  const [debouncedLang, setDebouncedLang] = useState(language)
+  
+  // Watch language, text here
+  useEffect(() => {
+    const timerID = setTimeout(() => {
+      setDebouncedLang(language)
+      setDebouncedText(text)
+    },1000)
+
+    return () => clearTimeout(timerID)
+  }, [language, text])
+
+
+  // Use debounced language, text to set translation
+  useEffect(() => {
+    const doTranslation = async() => {
+      const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+        params: {
+          q: debouncedText,
+          target: debouncedLang.value,
+          key: config.GRIDER_KEY
+        }
+      });
+      setTranslated(data.data.translations[0].translatedText)
+    }
+    doTranslation()
+  }, [debouncedLang, debouncedText])
+
+
+  return (
+    <div>
+      <h1 className="ui header">
+        {translated}
+      </h1>
+    </div>
+  );
+}
+
+export default Convert;
