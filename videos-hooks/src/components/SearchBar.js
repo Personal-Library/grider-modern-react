f4c7916b3fea94a0eeeb9670d45221f2716255e6
Anthony Bui <76550986+anthonybui94@users.noreply.github.com>
2021-01-27 09:03:57 -0600

    Sections 14-15 completed

---
+++ videos-hooks/src/components/SearchBar.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const SearchBar = (props) => {
+	const [term, setTerm] = useState('')
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		props.search(term)
+		setTerm('')
+	}
+
+	return (
+			<div className="search-bar ui segment" style={{ marginTop: '20px' }}>
+				<form className="ui form" onSubmit={handleSubmit}>
+					<div className="field">
+						<label>Video Search</label>
+						<input
+							type="text"
+							value={term}
+              onChange={(e) => setTerm(e.target.value)}
+						/>
+					</div>
+				</form>
+			</div>
+	);
+}
+
+export default SearchBar;
+
---
+++ videos-hooks/src/components/VideoItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './VideoItem.css';
+
+const VideoItem = (props) => {
+  return (
+    <div className="video-item item">
+      <img 
+        alt={props.video.snippet.title}
+        className="ui image"
+        src={props.video.snippet.thumbnails.medium.url}
+        onClick={() => props.onVideoSelect(props.video)}
+      />
+      <div className="content">
+        <p className="header" >{props.video.snippet.title.substring(0,50)}</p>
+        <p className="description" >{props.video.snippet.channelTitle}</p>
+      </div>
+    </div>
+  );
+}
+
+export default VideoItem;
